004d447e16bcc20a40da6f8c8e4c3ce6a0e4c4dd
sufiyan-ali-au49 <109080778+sufiyan-ali-au49@users.noreply.github.com>
2022-12-27 16:49:01 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Mongoose = require("mongoose");
 
-const localDB = `mongodb://localhost:27017/role_auth`;
+const localDB = `mongodb://127.0.0.1:27017/role_auth`;
 
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
